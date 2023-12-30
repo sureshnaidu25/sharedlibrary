a386fcc13b3d0ce8f36a3bf21b9d4ae675f0bfae
sureshnaidu25 <154795771+sureshnaidu25@users.noreply.github.com>
2023-12-31 01:10:44 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,4 +1,19 @@
-def gitdownload(repo)
+def newDownload(repo)
 {
-  git "https://github.com/intelliqittrainings/$(repo).git"
+   git "https://github.com/intelliqittrainings/${repo}"
+}
+
+def newBuild()
+{
+   sh 'mvn package'
+}
+
+def newDeploy(jobname,ip,appname)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+
+def runSelenium(jobname)
+{
+sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
